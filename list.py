43262e9305ddb7243b0750b8960183206de68035
dyotahutomo <dyotahutomo@gmail.com>
2021-03-26 10:43:19 +0700

    Pertemuan Keempat

---
+++ list.py
@@ -0,0 +1,11 @@
+mylist = []
+
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+mylist.append(100)
+
+# print 1 list aja
+print(mylist)
+# mengetahui jumlah banyak data
+# print(len(mylist[2]))
